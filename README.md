3d17a9fe8efbda6fd55d16239fb70c7ceaff3aad
xesam <xesamguo@gmail.com>
2021-10-15 15:48:03 +0800

    增加自定义处理函数

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ assert.deepEqual(url('https://admin:root@www.chelaile.net.cn:80/abc/def?name=xes
     hash: 'fragment?a=b#c=d'
 }, 'error');
 ```
+
+## Changelog
+
+### 20211015
+
+1. 增加自定义处理函数。
+
+### 20210901
+
+1. 使用正则处理url
--- src/url.js
+++ src/url.js
@@ -1,12 +1,12 @@
 const pattern = /^(?:([^:]*?):\/\/(?:([^:@]*?:[^:@]*)@)?(?:([^:\/\?#]*)?(?::(\d+))?))?(\/[^#\?]*)?(?:\?([^#]*))?(?:#(.*))?/;
 
-function parse(url) {
+function parse(url, process = x => x) {
     if (!url) {
         return {};
     }
     const matches = url.match(pattern) || [];
 
-    return {
+    const components = {
         scheme: matches[1],
         auth: matches[2],
         host: matches[3],
@@ -15,6 +15,13 @@ function parse(url) {
         query: matches[6],
         hash: matches[7]
     };
+    return process(components);
 }
+
+parse.create = function (process) {
+    return function (url) {
+        return parse(url, process);
+    }
+};
 
 module.exports = parse;
